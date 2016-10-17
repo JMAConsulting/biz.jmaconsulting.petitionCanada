8598fea7a94e26a0c7a35ac9c2caa8d4cc7c571a
Edsel <edsel.lopez@jmaconsulting.biz>
2016-10-17 14:11:56 +0530

    MISC-116 AJAX return function for representatives

---
+++ CRM/Ca/Page/Representatives.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.7                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2016                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2016
+ * $Id$
+ *
+ */
+class CRM_Ca_Page_Representatives extends CRM_Core_Page {
+
+  function run() {
+    $geocode = $_POST['geocode'];
+    $representatives = array();
+    $url = ENDPOINT . "/representatives/?point=" . $geocode[0] . "," . $geocode[1];
+
+    $representatives = CRM_Ca_BAO_Represent::getInfo($url);
+    if (!empty($representatives) && $representatives->meta->total_count > 0) {
+      echo json_encode($representatives->objects);
+    }
+    else {
+      echo 0;
+    }
+    exit;
+  }
+
+}
